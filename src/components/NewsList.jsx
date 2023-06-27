4620752a9014941affa179c944d52c3802f57802
TangiLC <tangi@le-cadre.net>
2023-06-27 16:24:56 +0200

    fetch

--- src/components/NewsList.jsx
+++ src/components/NewsList.jsx
@@ -7,22 +7,26 @@ const LatestNews = () => {
 
   useEffect(() => {
         
-            fetch("http://localhost:3000/v1/news/miniature")
-              .then(response => response.json())
-              .then(data => setMiniNewsList(data))
-              .catch(error => console.error("erreur de fetch :",error))
+    fetch("http://localhost:3000/v1/news/miniature")
+    .then(response => response.json())
+    .then(data => {
+        setMiniNewsList(data.miniatures);
+        console.log(data.miniatures);
+        console.log(miniNewsList);
+     })
+    .catch(error => console.log("erreur de fetch :",error))
 
   }, []);
 
   return (
     <div className="latest-news">
       <h2>Latest News of the week</h2>
-      {miniNewsList.miniatures.map((miniNews) => (
+      {miniNewsList && miniNewsList.map((miniNews) => (
         <div className="news-item" key={miniNews.appid+miniNews.date}>
           <h3 className="news-name">{miniNews.appName}</h3>
           <p className="news-date">Date : {miniNews.date}</p>
-          <p className="news-content">{miniNews.content}</p>
-        </div>
+          <p className="news-content">{miniNews.contents}</p>
+        </div> 
       ))}
     </div>
   );
