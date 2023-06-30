c542d7389cb7c6b76fc0c7979030ceec88bc606a
TangiLC <tangi@le-cadre.net>
2023-06-30 14:59:42 +0200

    article css

--- src/components/NewsArticle.jsx
+++ src/components/NewsArticle.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from "react-router-dom";
-import '../styles/GameList.css';
+import '../styles/ArticleList.css';
 
 import GamePage from '../pages/GamePage';
 import { SearchQuery } from '../App';
@@ -54,9 +54,10 @@ useEffect(() => {
     else {return (`<img src='${url}' alt='game'>`)}
   }
 
-  function showArticle(newsNb){
-    if (isLoading){return(<div><Loader /></div>)}
-    else {
+  async function createList(newsNb){
+    await new Promise(resolve => setTimeout(resolve, 9000))
+    .then(() =>{
+        
     let prev; let nxt;
     (newsNb===0)?prev=articleList.length-1:prev=newsNb-1;
     (newsNb===articleList.length-1)?nxt=0:nxt=newsNb+1;
@@ -66,23 +67,33 @@ useEffect(() => {
     myList.push(articleList[newsNb]);
     myList.push(articleList[nxt]);
     console.log(myList);
+    return myList;
+    })
+    
+    }
 
-    return(
-        <div className="news-item" key={myList[1].appid+myList[1].date} onClick={() => handlePageChange(myList[1].appId,myList[1].appName)}>
-          <div className="news-header"><h3 className="news-index">{newsNb}</h3>
-            <h2 className="news-name">{myList[1].appName}</h2>
+function showArticle(index){
+    let article=createList(index)[1];
+    
+    return(<>
+        {(article===undefined)?(<div><Loader /></div>):(
+        <div className="news-item" key={article.appid+article.date} onClick={() => handlePageChange(article.appId,article.appName)}>
+          <div className="news-header"><h3 className="news-index">{index}</h3>
+            <h2 className="news-name">{article.appName}</h2>
             <div className="news-date">
-                <div>&#91;{myList[1].author}&#93;</div>
-                <div>{formatDate(myList[1].date*1000)}</div>
+                <div>&#91;{article.author}&#93;</div>
+                <div>{formatDate(article.date*1000)}</div>
             </div>
           </div>
-          <div className="news-title">{myList[1].title}</div>
-          <div className="news-content" dangerouslySetInnerHTML={{ __html: myList[1].contents}}></div>
-          <div className="game-img">{myList[1].images.map((img) => (
+          <div className="news-title">{article.title}</div>
+          <div className="news-content" dangerouslySetInnerHTML={{ __html: article.contents}}></div>
+          <div className="game-img">{article.images.map((img) => (
             <span className ='bottomImg' dangerouslySetInnerHTML={{ __html: processImg(img)}}/>)
           )}  </div>
-        </div>)}
-    }
+        </div>)}</>
+    )
+}
+    
  
 return (  
     <div>
@@ -95,14 +106,14 @@ return (
             <div className="article-prevNext">
                 <div><img src={LeftArrow} alt="previous" className="article-svg" /></div>
                 <div className="prevNext-content">
-
+                    PREVIOUS
                 </div>
             </div>
             <div className="article-svg">
                 <img src={articleSvg} alt="article" className="article-svg" /></div>
             <div className="article-prevNext">
                 <div className="prevNext-content">
-
+                    NEXT
                 </div>
                 <div><img src={RightArrow} alt="next" className="article-svg" /></div>
             </div>
